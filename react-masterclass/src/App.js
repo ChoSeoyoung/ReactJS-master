da6aa89c53f9618b97a7e196d7879328b06c370e
Choseoyoung <seoyoung0000@naver.com>
2022-12-04 18:43:42 +0900

    As and Attrs

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -3,23 +3,26 @@ import styled from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
-const Box = styled.div`
-  background-color: ${(props)=>props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-const Text = styled.span`
+const Btn = styled.button`
   color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+const Input = styled.input.attrs({required: true, minLength:10})`
+  background-color: tomato;
 `;
 
 function App() {
   return(
-  <Father>
-    <Box bgColor="teal"><Text>Hello</Text></Box>
-    <Circle bgColor="tomato"/>
+  <Father as="header">
+    <Btn>Login</Btn>
+    <Btn as="a">Login</Btn>
+    <Input />
+    <Input />
+    <Input />
+    <Input />
+    <Input />
   </Father>)
 }
 
