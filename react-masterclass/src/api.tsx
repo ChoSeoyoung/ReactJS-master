fd0d7610d67d9be4349019cc189fde6daee435f9
Choseoyoung <seoyoung0000@naver.com>
2022-12-08 13:03:46 +0900

    Charts

--- react-masterclass/src/api.tsx
+++ react-masterclass/src/api.tsx
@@ -9,3 +9,8 @@ export function fetchCoinInfo(coinId:string){
 export function fetchPriceInfo(coinId:string){
     return fetch(`${BASE_URL}/tickers/${coinId}?quotes=KRW`).then(response=>response.json());
 }
+export function fetchCoinHistory(coinId:string){
+    //const endDate = Math.floor(Date.now()/1000);
+    //const startDate = endDate-60*60_24*6; //일주일전
+    return fetch(`https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`).then(response=>response.json());
+}
--- react-masterclass/src/routes/Chart.tsx
+++ react-masterclass/src/routes/Chart.tsx
@@ -1,6 +1,65 @@
-function Chart(){
+import { useParams } from "react-router-dom";
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+import styled from "styled-components";
+
+const CoinWrapper = styled.div`
+    border: 1px solid black;
+`;
+
+interface ChartProps {
+    coinId: string;
+}
+interface IHistorical {
+    time_open: number;
+    time_close: number;
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    volume: number;
+    market_cap: number;
+}
+function Chart({coinId} : ChartProps){
+    const {isLoading,data} = useQuery<IHistorical[]>(["ohlcv",coinId],()=>fetchCoinHistory(coinId));
     return(
-        <h1>Chart</h1>
+        <CoinWrapper>
+            {isLoading ? "Loading...":
+            <ApexChart 
+                type="line"
+                series={[
+                    {
+                        name: "prices",
+                        data: data?.map(price  => price.close) as number[]
+                    },
+                ]}
+                options={{
+                    theme: {
+                        mode:"light"
+                    },
+                    chart:{
+                        height: 900,
+                        width: 900,
+                    },
+                    stroke:{
+                        curve: "smooth",
+                        width: 4,
+                    },
+                    yaxis:{
+                        labels:{show:false},
+                    },
+                    xaxis:{
+                        axisBorder: {show:false},
+                        axisTicks: {show:false},
+                        labels: {show:false},
+                    },
+                    fill: {
+                        type: "gradient",
+                    }
+                }}>
+            </ApexChart>}
+        </CoinWrapper>
     );
 }
 
